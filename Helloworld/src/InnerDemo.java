21e77184efacfbd9519f6dba7abd292eb75cfa4c
thadu <thadu@Charan123>
2026-08-05 11:53:03 +0530

    Inner Demo

---
+++ Helloworld/src/InnerDemo.java
@@ -0,0 +1,17 @@
+class Outer{
+	int m = 100;
+	class Inner{
+		void doStuff() {
+			System.out.println("m = "+m);
+			
+		}
+	}
+}
+public class InnerDemo {
+
+	public static void main(String[] args) {
+		Outer.Inner innerObj = new Outer().new Inner();
+		innerObj.doStuff();
+	}
+
+}
