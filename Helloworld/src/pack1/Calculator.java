55bc355f625423a1497b61aafb7a1ee196ecf11c
thadu <thadu@shyama123>
2026-09-08 13:04:21 +0530

    commit

--- Helloworld/src/pack1/Calculator.java
+++ Helloworld/src/pack1/Calculator.java
@@ -2,9 +2,11 @@ package pack1;
 
 public class Calculator {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public int add(int n1,int n2) {
+		return n1+n2;
+	}
+	public int multiply(int n1,int n2){
+		return n1*n2;
 	}
 
 }
